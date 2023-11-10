08a5db8d684648542256988fa8434003e9343f56
Shalvex Novachrono <55297590+ShalvexNovachrono@users.noreply.github.com>
2023-11-10 23:32:11 +0000

    Create main.java

---
+++ main.java
@@ -0,0 +1,45 @@
+package main;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        boolean NotPhoneNumber = false;
+        int count = 0;
+        Outer:
+        while (true) {
+            System.out.print("Phone Number:\n>>> ");
+            String myInputVal = myObj.nextLine();
+            if (myInputVal.length() == 11) {
+                char[] myInputValChar = myInputVal.toCharArray();
+                Inner:
+                for (int i = 0; i < myInputVal.length(); i++ ) { 
+                    char character = myInputValChar[i];
+                    int icv = (int) character;
+                    if (icv >= 48 && icv <= 57) {
+                        count++;
+                    } else {
+                        NotPhoneNumber = true;
+                        System.out.println("Make sure its a phone number.");
+                        break Inner;
+                    }
+                }  
+                if (count >= 10) {
+                    System.out.println("Access granted.");
+                    break Outer;
+                }
+            } else {
+                System.out.println("Needs to be 11 characters long.");
+            }
+  
+        }
+        
+    }
+    
+}
